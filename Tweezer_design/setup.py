67635b8ffc2690642b22aebde8278f05d2238948
AntoineRiaud <antoine_riaud@outlook.com>
2017-05-29 22:35:38 +0200

    Add files via upload

--- Tweezer_design/setup.py
+++ Tweezer_design/setup.py
@@ -8,10 +8,11 @@ Created on Sun Apr 03 21:53:14 2016
 from distutils.core import setup
 
 setup(name='Acoust_Tweezers_Anisotropic',
-      version='2.0',
+      version='2.2',
       description='Acoust_tweezers design package for anisotropic media',
       author='Antoine Riaud, Jean-Louis Thomas, Michael Baudoin, Olivier Bou Matar',
       author_email='antoine.riaud@gmail.com',
       packages=['Tweezer_design'],
+      url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
       data_files=[('',['Tweezer_design/LiNbO3.mat','Tweezer_design/reticule.svg','Tweezer_design/mirror.svg'])],
      )
